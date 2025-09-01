9ccd28eccb77c1b6383ec97f13d10e6997d54110
mohsensami <mohsensami18727@gmail.com>
2025-09-01 21:45:13 +0330

    Home page added - UI

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    domains: [
+      "localhost",
+      "localhost:3000",
+      "res.cloudinary.com",
+      "192.168.0.218",
+      "online-grocery-store-strapi-cms.onrender.com",
+      "picsum.photos",
+      "images.unsplash.com",
+    ],
+  },
 };
 
 export default nextConfig;
