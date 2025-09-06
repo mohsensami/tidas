1f07555f49bbf208a4df9cc916b714a84dcb4010
mohsensami <mohsensami18727@gmail.com>
2025-09-06 21:48:51 +0330

    Simple products and product list in home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,16 @@
-const delay = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "../../../db/sample-data";
 
-const Homepage = async () => {
-  await delay(2000);
-  return <>برگه نخست</>;
+const HomePage = () => {
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
 };
-
-export default Homepage;
+export default HomePage;
