7b049023e0ac3e5a5c6423d7ae8f918619ab5050
mohsensami <mohsensami18727@gmail.com>
2025-09-09 21:55:42 +0330

    Smooth ui with useTransition hook

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Minus, Plus } from "lucide-react";
+import { Loader, Minus, Plus } from "lucide-react";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { Cart, CartItem } from "@/types";
 import toast from "react-hot-toast";
+import { useTransition } from "react";
 
 const AddToCart = ({
   cart,
@@ -15,43 +16,47 @@ const AddToCart = ({
 }) => {
   const router = useRouter();
 
+  const [isPending, startTransition] = useTransition();
+
+  // Add item to cart
   const handleAddToCart = async () => {
-    // Execute the addItemToCart action
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      // Execute the addItemToCart action
+      const res = await addItemToCart(item);
 
-    // Display appropriate toast message based on the result
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
+      // Display appropriate toast message based on the result
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    // toast.success(`${item.name} added to the cart`);
-
-    toast.success((t) => (
-      <div>
-        <span>{`${item.name} added to the cart`}</span>
-        <div className="text-center">
-          <Button size="sm" onClick={() => toast.dismiss(t.id)}>
-            سبد خرید
-          </Button>
+      toast.success((t) => (
+        <div>
+          <span>{`${item.name} added to the cart`}</span>
+          <div className="text-center">
+            <Button size="sm" onClick={() => toast.dismiss(t.id)}>
+              سبد خرید
+            </Button>
+          </div>
         </div>
-      </div>
-    ));
-
-    // router.push("/cart");
+      ));
+      // router.push("/cart");
+    });
   };
 
   // Remove item from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    // toast({
-    //   variant: res.success ? "default" : "destructive",
-    //   description: res.message,
-    // });
-    toast.success(res.message);
+      // toast({
+      //   variant: res.success ? "default" : "destructive",
+      //   description: res.message,
+      // });
+      toast.success(res.message);
 
-    return;
+      return;
+    });
   };
 
   const existItem =
@@ -59,17 +64,44 @@ const AddToCart = ({
 
   return existItem ? (
     <div>
-      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="w-4 h-4" />
+      <Button
+        type="button"
+        variant="outline"
+        disabled={isPending}
+        onClick={handleRemoveFromCart}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4  animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
-      <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="w-4 h-4" />
+      <Button
+        type="button"
+        variant="outline"
+        disabled={isPending}
+        onClick={handleAddToCart}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
       </Button>
     </div>
   ) : (
-    <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus className="w-4 h-4" />
+    <Button
+      className="w-full"
+      type="button"
+      disabled={isPending}
+      onClick={handleAddToCart}
+    >
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4" />
+      )}
       Add to cart
     </Button>
   );
