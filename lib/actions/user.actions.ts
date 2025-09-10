ab96d0cad1abae3bdb44e82444aee68f74020321
mohsensami <mohsensami18727@gmail.com>
2025-09-10 20:26:39 +0330

    PAyment method action and zod schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { auth, signIn, signOut } from "@/auth";
 import {
+  paymentMethodSchema,
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
@@ -11,6 +12,7 @@ import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
+import z from "zod";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -106,6 +108,33 @@ export async function updateUserAddress(data: ShippingAddress) {
       where: { id: currentUser.id },
       data: { address },
     });
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session: any = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user.id! },
+    });
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
     return {
       success: true,
       message: "User updated successfully",
