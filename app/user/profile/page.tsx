88dbd6feb721fce94c953dad733cc06458be9628
mohsensami <mohsensami18727@gmail.com>
2025-09-11 18:38:44 +0330

    Update profile form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,5 +1,20 @@
-const Profile = () => {
-  return <>Profile</>;
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+
+export const metadata: Metadata = {
+  title: "پروفایل کاربری",
 };
 
-export default Profile;
+export default async function ProfilePage() {
+  const session: any = await auth();
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md  mx-auto space-y-4">
+        <h2 className="h4-bold">پروفایل کاربری</h2>
+        Test User: {session.user.name}
+      </div>
+    </SessionProvider>
+  );
+}
