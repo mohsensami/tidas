f090cf3a8593e842beba4f3edcd47f0067b6fce9
mohsensami <mohsensami18727@gmail.com>
2025-09-11 18:21:31 +0330

    pagination onClick by query-string

--- components/shared/pagination.tsx
+++ components/shared/pagination.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "../ui/button";
+import { formUrlQuery } from "@/lib/utils";
 
 type PaginationProps = {
   page: number | string;
@@ -12,12 +13,26 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  // Handle Page Change
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === "next" ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || "page",
+      value: pageValue.toString(),
+    });
+
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
-    <div className="flex gap-2 mt-4">
+    <div className="flex gap-2 mt-8">
       <Button
         size="lg"
         variant="outline"
         className="w-28"
+        onClick={() => onClick("prev")}
         disabled={Number(page) <= 1}
       >
         قبلی
@@ -26,6 +41,7 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
         size="lg"
         variant="outline"
         className="w-28"
+        onClick={() => onClick("next")}
         disabled={Number(page) >= totalPages}
       >
         بعدی
