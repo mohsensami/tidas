dbfe81399f18a09ec7b5927f5fbceaeef0c2bc3e
mohsensami <mohsensami18727@gmail.com>
2025-09-12 17:28:08 +0330

    Add product image

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -25,6 +25,7 @@ import slugify from "slugify";
 import { insertProductSchema, updateProductSchema } from "@/lib/validators";
 import { Textarea } from "../ui/textarea";
 import toast from "react-hot-toast";
+import { UploadButton } from "@/lib/uploadthing";
 
 const ProductForm = ({
   type,
@@ -74,6 +75,8 @@ const ProductForm = ({
       }
     }
   };
+
+  const images = form.watch("images");
 
   return (
     <Form {...form}>
@@ -239,6 +242,43 @@ const ProductForm = ({
         </div>
         <div className="upload-field flex flex-col gap-5 md:flex-row">
           {/* Images */}
+          <FormField
+            control={form.control}
+            name="images"
+            render={() => (
+              <FormItem className="w-full">
+                <FormLabel>Images</FormLabel>
+                <Card>
+                  <CardContent className="space-y-2 mt-2 min-h-48">
+                    <div className="flex-start space-x-2">
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt="product image"
+                          className="w-20 h-20 object-cover object-center rounded-sm"
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                      <FormControl>
+                        <UploadButton
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res: { url: string }[]) => {
+                            form.setValue("images", [...images, res[0].url]);
+                          }}
+                          onUploadError={(error: Error) => {
+                            toast.error(`ERROR! ${error.message}`);
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </CardContent>
+                </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="upload-field">{/* Is Featured */}</div>
         <div>
