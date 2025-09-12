ad97c73c2285a75c15d48325d35581bb55e79580
mohsensami <mohsensami18727@gmail.com>
2025-09-12 11:02:20 +0330

    Admin orders page amd action

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,92 @@
+import { auth } from "@/auth";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+import { requireAdmin } from "@/lib/auth-guard";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Pagination from "@/components/shared/pagination";
+
+export const metadata: Metadata = {
+  title: "سفارشات",
+};
+
+const OrdersPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  await requireAdmin();
+  const { page = "1" } = await props.searchParams;
+
+  const session = await auth();
+  if (session?.user.role !== "admin")
+    throw new Error("admin permission required");
+
+  const orders = await getAllOrders({
+    page: Number(page),
+  });
+
+  console.log(orders);
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h3-bold">سفارشات</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-right">#</TableHead>
+              <TableHead className="text-right">تاریخ</TableHead>
+              <TableHead className="text-right">جمع</TableHead>
+              <TableHead className="text-right">وضعیت پرداخت</TableHead>
+              <TableHead className="text-right">وضعیت ارسال</TableHead>
+              <TableHead className="text-right">عملیات</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : "پرداخت نشده"}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : "تحویل داده نشده"}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>جزئیات</Link>
+                  </Button>
+                  {/* DELETE */}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default OrdersPage;
