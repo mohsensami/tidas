fa140a1db1bb746832ca6402f6c018fdca1c615d
mohsensami <mohsensami18727@gmail.com>
2025-09-14 19:24:03 +0330

    Featured product carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
+import { ProductCarousel } from "@/components/shared/product/product-carousel";
 import ProductList from "../../components/shared/product/product-list";
 import sampleData from "../../db/sample-data";
-import { getLatestProducts } from "../../lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "../../lib/actions/product.actions";
 
 const HomePage = async () => {
   const latestProducts: any = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <div className="space-y-8">
-      <ProductList title="جدیدترین محصولات" data={latestProducts} limit={4} />
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+
+      <ProductList title="جدیدترین محصولات" data={latestProducts} />
     </div>
   );
 };
