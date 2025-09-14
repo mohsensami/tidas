4084d04b843d00797c5a2df1fb36d9e4b750c1cc
mohsensami <mohsensami18727@gmail.com>
2025-09-14 20:50:21 +0330

    Dynamic metadata

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -32,6 +32,41 @@ const prices = [
 const ratings = [4, 3, 2, 1];
 const sortOrders = ["newest", "lowest", "highest", "rating"];
 
+export async function generateMetadata(props: {
+  searchParams: Promise<{
+    q: string;
+    category: string;
+    price: string;
+    rating: string;
+  }>;
+}) {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await props.searchParams;
+
+  const isQuerySet = q && q !== "all" && q.trim() !== "";
+  const isCategorySet =
+    category && category !== "all" && category.trim() !== "";
+  const isPriceSet = price && price !== "all" && price.trim() !== "";
+  const isRatingSet = rating && rating !== "all" && rating.trim() !== "";
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `جستجو ${isQuerySet ? q : ""}
+      ${isCategorySet ? `: دسته بندی ${category}` : ""}
+      ${isPriceSet ? `: قیمت ${price}` : ""}
+      ${isRatingSet ? `: رتبه ${rating}` : ""}`,
+    };
+  } else {
+    return {
+      title: "جستجوی محصولات",
+    };
+  }
+}
+
 const SearchPage = async (props: {
   searchParams: Promise<{
     q?: string;
