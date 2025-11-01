b10e11e848b033f7fae7c21330175939d8cd375b
mohsensami <mohsensami18727@gmail.com>
2025-11-01 23:04:52 +0330

    update map iframe

--- app/(root)/contact-us/page.tsx
+++ app/(root)/contact-us/page.tsx
@@ -66,7 +66,7 @@ export default function ContactUs() {
 
             <div className="rounded-2xl overflow-hidden border border-slate-200">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3237.000000!2d51.389000!3d35.689200!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQxJzIxLjAiTiA1McKwMjMnMjAuNiJF!5e0!3m2!1sen!2s!4v0000000000"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.3874434072186!2d51.479398599999996!3d35.6920822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e02843c668751%3A0x1bd9283979d055fb!2sPanorama%20Complex!5e0!3m2!1sen!2s!4v1762022622715!5m2!1sen!2s"
                 width="100%"
                 height="250"
                 style={{ border: 0 }}
