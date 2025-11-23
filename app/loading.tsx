1a8c0f29176decbe55d877ff1a32ac965bdb9660
mohsensami <mohsensami18727@gmail.com>
2025-11-23 21:58:54 +0330

    update loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,21 +3,8 @@ import loader from "@/assets/loading.svg";
 
 const Loading = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <Image
-        src={loader}
-        width={150}
-        height={150}
-        alt="در حال دریافت اطلاعات..."
-      />
+    <div className="flex justify-center items-center w-screen h-screen bg-gray-100">
+      <div className="text-4xl font-bold shine">Tidas Gold Galery</div>
     </div>
   );
 };
